1d99884a658ad4e4f62834f402234b7fc29e5aec
Giovanna <gmirellademellobento@gmail.com>
2026-03-19 10:33:20 -0300

    ......

--- Aula12Projeto/script.js
+++ Aula12Projeto/script.js
@@ -1,3 +1,5 @@
+const users = []
+
 class User{
 
     #id
@@ -6,16 +8,104 @@ class User{
     #password
     #active
 
-    constructor(id, name, email, password, active){
+    constructor(id, name, email, password){
+
+        if(!name || !email.includes('@') || password.lenght < 6){
+            return console.error("Ocorreu um erro. Confira novamente os dados.")
+        }
         this.#id = id
         this.#name = name
         this.#email = email
         this.#password = password
-        this.#active = active
+        this.#active = true
+        users.push({id, name, email, password, active :true})
     }
 
-    get id(){
-        return this.#id = this.#id
+    getId(){
+        return this.#id
+    }
+
+    getName(){
+        return this.#name
+    }
+
+    setName(name){
+        return !name ? console.error("Erro ao setar nome") : this.#name = name
+    }
+
+    getEmail(){
+        return this.#email
+    }
+
+    setEmail(email){
+        return !email.includes('@') ? console.error("Erro ao setar email.") : this.#email = email
+    }
+
+    setPassword(password){
+        return password.lenght < 6 ? console.error("Erro ao setar senha.") : this.#password = password
+    }
+
+    verifyPassword(password){
+        return password !== this.#password ? false : true
+    }
+
+    changeUserActivity(){
+        return this.#active === true ? this.#active = false : this.#active = true
+    }
+
+    verifyUserActivity(){
+        return this.#active === true ? true : false
+
+
+    }
+
+
+}
+
+class Client extends User{
+
+    constructor(id, name, email, password){
+        super(id, name, email, password)
+    }
+
+    showData(){
+        return{
+            id: this.getId(),
+            name: this.getName(),
+            email: this.getEmail(),
+            active: this.verifyUserActivity()
+        }
+    }
+
+    changeData(name, email, password){
+        this.setName(name)
+        this.setEmail(email)
+        if(this.verifyPassword == true){
+            return console.error("Não é possível alterar a senha pela mesma")
+        }
+        else{
+            this.setName(name)
+            this.setEmail(email)
+            this.setPassword(password)
+        }
     }
 }
+
+class Admin extends User{
+
+    constructor(id, name, email, password){
+        super(id, name, email, password)
+    }
+
+    showAllUsers(){
+        return users
+    }
+
+
+}
+
+const c1 = new Client(1, "Giovanna", "g-bento@gmail.com", "0108070")
+console.log(c1.showData());
+const a1 = new Admin(1, "Admin", "admin@gmail.com", "00895655")
+console.log(a1.showAllUsers())
 
